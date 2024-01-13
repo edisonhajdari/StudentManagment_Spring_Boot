8255f9973568d6742a7302240c4ea7bf3f7a1028
edisonhajdari <ediisonhajdari@gmail.com>
2024-01-13 14:24:14 +0100

    spring

---
+++ src/test/java/com/example/studentmanagment_spring_boot/StudentManagmentSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagment_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
